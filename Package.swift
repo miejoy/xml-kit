8315f854a0b4fc75de8d074a9e428c604f63a8cf
黄磊 <ray.musjoy@gmail.com>
2020-05-01 08:55:13 +0800

    修复 Linux 编译问题

--- Package.swift
+++ Package.swift
@@ -23,7 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MJXMLKit",
-            dependencies: []),
+            dependencies: [],
+            linkerSettings: [.unsafeFlags(["-lxml2"])]
+        ),
         .testTarget(
             name: "MJXMLKitTests",
             dependencies: ["MJXMLKit"]),
